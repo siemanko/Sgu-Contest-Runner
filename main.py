3998eb7d889916705c2a9c64231a8142b5231288
sidor <szymon.sidor@gmail.com>
2012-11-03 22:11:25 +0000

    results returned properly

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 from rank import *
 
 tasks = [ '168', '311', '304' ]
-users = [ 'Faceless', '|Andrei Heidelbacher|', 'Reza_H' ]
+users = [ 'Reza_H', 'Andrei Heidelbacher', 'vjudge5' ]
 
 def FileInCwd(name):
   return os.path.isfile(os.path.join(os.getcwd(), name))
@@ -50,15 +50,13 @@ def RemoveRedundant(records, sids):
 
 # test that our user did our task and tests finished and error was not funny
 def purify(records):
-  return records # remove this
-
-  
-  """
+  res = []
   # ADD CHECKING FOR TIME
   for row in records:
-    if row[1] in users and row[3] in tasks and row[4] in [0,2]:
-      yield row
-  """
+    if (row[1] in users) and (str(row[3]) in tasks) and (str(row[4]) in ['0','2']):
+      res.append(row)
+  return res
+
 
 if len(sys.argv) > 1:
   if sys.argv[1] == 'clean':
@@ -75,12 +73,19 @@ records_old = ReadRecords()
 sids_old = GetSids(records_old)
 records_new = RemoveRedundant(records_new, sids_old )
 
-records_new = purify(records_new)
-
 current_records = records_new + records_old
 
+
+current_records = purify(current_records)
+
 current_records = sorted(current_records, key = lambda x : int(x[0]))
+
+for r in current_records:
+  print r
+
 WriteRecords(current_records)
+
+
 
 rankg = RankGenerator()
 rankg.GenerateRanks(current_records, users, tasks)
--- rank.py
+++ rank.py
@@ -1,14 +1,25 @@
-
-
-UpdateTask
-
 # assuming records from lowest sid
 class RankGenerator:
+  def UpdateTask(self,task, ttime, stype):
+    if ('verdict' in task and task['verdict'] == 'Accepted'):
+      return
+    if stype == '2':
+      task['verdict'] = 'FuckUp'
+      if not 'bombs' in task:
+        task['bombs'] = 1
+      else:
+        task['bombs'] += 1
+    elif stype == '0':
+      task['verdict'] = 'Accepted'    
+      task['time'] = ttime # TODO: add bombs
+    else:
+      pass
+
   def GenerateRanks(self, records,users,tasks):
     udata = {}
     for u in users:
       udata[u] = {}
-      udata[u]['tasks'] = [{}] * len(tasks)
+      udata[u]['tasks'] = [{} for i in range(len(tasks))]
     taskno = {}
     lastno = 0
     for t in tasks:
@@ -16,5 +27,23 @@ class RankGenerator:
       lastno += 1
 
     for r in records:
-      UpdateTask(udata[r[1]]['tasks'][taskno[int(r[3])]],r[3], r[4])
+      self.UpdateTask(udata[r[1]]['tasks'][taskno[str(r[3])]],r[2], r[4])
       
+    res = []
+    for u in udata:
+      udata[u]['user'] = u
+      score = 0
+      totaltime = 0.0
+      for i in range(len(tasks)):
+        if ('verdict' in udata[u]['tasks'][i] and 
+            udata[u]['tasks'][i]['verdict'] == 'Accepted'):
+          score+=1
+          totaltime+=float(udata[u]['tasks'][i]['time'])
+      udata[u]['score'] = score
+      udata[u]['total_time'] = totaltime
+      res.append(udata[u])
+    res = sorted(res, key = lambda x: [x['score'], -x['total_time']])
+
+    for x in res:
+      print x, '\n\n'
+    return res
