47ded0c9fb4c4329d569b850db6aeb6161765b6a
sidor <szymon.sidor@gmail.com>
2012-11-03 23:32:20 +0000

    Solved count added.

--- rank.py
+++ rank.py
@@ -1,19 +1,22 @@
 # assuming records from lowest sid
 class RankGenerator:
-  def UpdateTask(self,task, ttime, stype):
+  # Returns true if task was recently solved
+  def UpdateTaskAndWasSolved(self,task, ttime, stype):
     if ('verdict' in task and task['verdict'] == 'Accepted'):
-      return
+      return False
     if stype == '2':
       task['verdict'] = 'FuckUp'
       if not 'bombs' in task:
         task['bombs'] = 1
       else:
         task['bombs'] += 1
+      return False
     elif stype == '0':
-      task['verdict'] = 'Accepted'    
+      task['verdict'] = 'Accepted'
       task['time'] = float(ttime) - float(self.config['start_ts'])
       if not 'bombs' in task:
         task['bombs'] = 0
+      return True
     else:
       pass
 
@@ -25,6 +28,7 @@ class RankGenerator:
     users = config['users']
     tasks = config['tasks']
     udata = {}
+    solvedby = [0] * len(config['tasks'])
     for u in users:
       udata[u] = {}
       udata[u]['tasks'] = [{} for i in range(len(tasks))]
@@ -35,7 +39,8 @@ class RankGenerator:
       lastno += 1
 
     for r in records:
-      self.UpdateTask(udata[r[1]]['tasks'][taskno[str(r[3])]],r[2], r[4])
+      if self.UpdateTaskAndWasSolved(udata[r[1]]['tasks'][taskno[str(r[3])]],r[2], r[4]):
+        solvedby[taskno[str(r[3])]] += 1
       
     res = []
     for u in udata:
@@ -51,8 +56,13 @@ class RankGenerator:
       udata[u]['score'] = score
       udata[u]['total_time'] = totaltime
       res.append(udata[u])
-    res = sorted(res, key = lambda x: [x['score'], -x['total_time']])
+    res = sorted(res, key = lambda x: [-x['score'], x['total_time']])
 
     for x in res:
       print x, '\n\n'
-    return res
+    for x in solvedby:
+      print x
+    return {
+              'ranks': res,
+              'solution_count': solvedby
+           }
